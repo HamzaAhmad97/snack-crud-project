d1c8468273aba5ad3ea30290503035edfcef56cd
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-11-16 15:04:43 +0200

    feat: added referenced templates to views

--- snacks/views.py
+++ snacks/views.py
@@ -7,20 +7,25 @@ from .models import Snack
 class SnackListView(TemplateView):
     model = Snack
     context_object_name = 'snack_list'
+    template_name = 'snack_list.html'
 
 class SnackDetailView(DetailView):
     model = Snack
     context_object_name = 'snack_details'
+    template_name = 'snack_detail.html'
 
 class SnackCreateView(CreateView):
     model = Snack
     fields = ['title', 'purchaser', 'description']
+    template_name = 'snack_create.html'
     
 class SnackUpdateView(UpdateView):
     model = Snack
     fields = ['title', 'description']
+    template_name = 'snack_update.html'
 
 class SnackDeleteView(DeleteView):
     model = Snack
     success_url = reverse_lazy('snack_list')
+    template_name = 'snack_delete.html'
     
