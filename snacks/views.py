1ae9b66b1f07b53debddc40ac8b8b0d267333878
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-11-16 17:26:22 +0200

    feat: populated all templates and styled them

--- snacks/views.py
+++ snacks/views.py
@@ -1,31 +1,31 @@
-from django.views.generic import TemplateView, DetailView
+from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.urls import reverse_lazy
 from .models import Snack
 
 
-class SnackListView(TemplateView):
+class SnackListView(ListView):
     model = Snack
     context_object_name = 'snack_list'
-    template_name = 'snack_list.html'
+    template_name = 'snacks/snack_list.html'
 
 class SnackDetailView(DetailView):
     model = Snack
     context_object_name = 'snack_details'
-    template_name = 'snack_detail.html'
+    template_name = 'snacks/snack_detail.html'
 
 class SnackCreateView(CreateView):
     model = Snack
     fields = ['title', 'purchaser', 'description']
-    template_name = 'snack_create.html'
+    template_name = 'snacks/snack_create.html'
     
 class SnackUpdateView(UpdateView):
     model = Snack
     fields = ['title', 'description']
-    template_name = 'snack_update.html'
+    template_name = 'snacks/snack_update.html'
 
 class SnackDeleteView(DeleteView):
     model = Snack
     success_url = reverse_lazy('snack_list')
-    template_name = 'snack_delete.html'
+    template_name = 'snacks/snack_delete.html'
     
